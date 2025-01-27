2e6855f6951892cd76b6f74c54c03d0193ca032d
jdu214843 <214843a@jdu.uz>
2025-01-27 12:37:54 +0500

    updated

--- ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
+++ ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8d055b45575512c20e1832d146bb4a405abac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
